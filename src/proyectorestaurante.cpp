7a5c200a42162abd8770b647f061158c2cf64d2b
Juanrf <juanr_fuentes@hotmail.com>
2020-07-22 22:38:24 -0600

    cambio al proyecto

--- src/proyectorestaurante.cpp
+++ src/proyectorestaurante.cpp
@@ -2,30 +2,40 @@
 #include<string>
 #include <iomanip>
 using namespace std;
-void saludo(string saludar){
-    //RESUARANTE PROYECTO FINAL
-
-    cout<<"**Bienvenido a RESTAURANTE EL DELFIN**"<<saludar<<endl;
-}
-void listademesas(int numerodemesa){
-    int mesas=20;
-    cout<<"Listado de mesas disponibles y reservadas"<<endl;
-    for (int mesa = 1; mesa <+20; mesa++)
+double menu(double platillos){
+    int acumulador, contador, cantidad, opcion, precio;
+    double menu=opcion;
+    char seguir;
+    do
     {
-        cout<<"mesa"<<mesa<< ": disponible"<<endl;
-        cout<<setw(10)<<mesas<<endl;
-        if (mesa%10)
+        cout<<"[1]- 5 tacos, arroz y frigoles-------------------LPS.80"<<endl;
+        cout<<"[2]-Enchiladas con queso, arroz y frioles--------LPS.100"<<endl;
+        switch (opcion)
         {
-            cout<<endl;
+        case 1:
+        cout<<"La orden es: "<<endl;
+        acumulador+=cantidad;
+        contador++;
+            cout<<"Desea seguir comprando: "<<endl;
+            cin >>seguir;
+            break;
+        case 2:
+        cout<<"La orden es: "<<endl;
+        acumulador+=cantidad;
+        contador++;
+            cout<<"Desea seguir comprando: "<<endl;
+            cin >>seguir;
+        default:
+            break;
         }
-    }  
+    } while (seguir!='n');
+    return menu;
 }
 
 int main(){
-    int opcion,numerodemesa;
-    string saludar,nombre;
-
-    saludo(saludar);
+    int opcion,Nmesa, platillos, saludo;
+    string nombre;
+    cout<<"******BIENVENIDO A RESTAURANTE EL DELFIN*******"<<endl;
     cout<<"Por favor ingrese la opcion segun sea su caso:"<<endl;
     cout<<"1.Ya cuenta con reservacion"<<endl;
     cout<<"2.No cuenta con reservacion"<<endl;
@@ -35,14 +45,26 @@ int main(){
         {
         case 1:
             cout<<"A nombre de quien esta la reservacion:"<<endl;
-            getline(cin,nombre);
+            cin >>nombre;
+            if (nombre=="bertha")
+            {
+                cout<<"La reservacion esta a nombre de bertha, la mesa asignada es:"<<endl;
+            }
+            else
+            {
+                cout<<"Esa reservacion no existe"<<endl;
+            }
+            cout<<menu(platillos)<<endl;
             break;
         case 2:
-           listademesas(numerodemesa);
-           cout<<"Ingrese el numero de mesa que desea: "<<endl;
-           cin>>numerodemesa;
+           cout<<"Las mesas disponiles son: ";
+           cout<<"Que numero de mesa desea: "<<endl;
+           cin >>Nmesa;
            break;
 
+        case 3:
+           cout<<"Que desea para llevar: ";
+           cout<<menu(platillos)<<endl;
         default:
             cout<<"Opcion no valida"<<endl;
             break;
